f49e8caa52350f42f03194f127d7c394a8d8506f
Muystein <mherrera-2021320@kinal.edu.gt>
2023-08-21 07:17:45 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { showDatos } from "./showDatos";
+
+function App() {
+  return (
+    <BrowserRouter >
+      <Routes >
+        <Route path='' element=''  ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/showDatos.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap'
+import { show_alerta } from "./funcions";
+
+const showDatos = () => {
+    const url='http://localhost:8080/api'
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default showDatos
